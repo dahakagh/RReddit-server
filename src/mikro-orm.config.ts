fd79c7bdaa8e603fce5c0efa5ccfa4ddf24ed61e
dahakagh <Marshev123@yandex.ru>
2020-12-14 14:00:14 +0300

    create User entity and User Resolver

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,15 @@
 import { __prod__ } from "./constants";
 import { Post } from "./enitites/Post";
 import { MikroORM } from "@mikro-orm/core";
-import path from 'path'
+import path from "path";
+import { User } from "./enitites/User";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   user: "postgres",
   password: "postgres",
